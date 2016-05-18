52870ae31803223702c81b27ca14f00e32adae2a
Tony556 <leorijn223@gmail.com>
2016-05-18 14:50:15 +0000

    Added to String converters to startGunicornCTFd.py; Made the same script run with SUDO to allow use of port 80, and other sudo-required ports

--- script.py
+++ script.py
@@ -81,10 +81,10 @@ def gunicornInstall():
     gunicorn_file = '''import subprocess
 rawinput = raw_input('What port would you like Gunicorn to run on? Default port: 8000\n')
 if(rawinput==''):
-    portToUse = 8000
+    portToUse = str(8000)
 else:
-    portToUse = rawinput
-subprocess.call('gunicorn --bind 0.0.0.0:'+portToUse+' -w 1 "CTFd:create_app()"', shell=True)'''
+    portToUse = str(rawinput)
+subprocess.call('sudo gunicorn --bind 0.0.0.0:'+portToUse+' -w 1 "CTFd:create_app()"', shell=True)'''
 
     with open('CTFd-master/startGunicornCTFd.py', 'w+') as f:
         f.write(gunicorn_file)
