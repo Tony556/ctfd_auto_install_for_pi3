8e57314068e4ca7ea19c28367c426a26958da2cc
Tony556 <leorijn223@gmail.com>
2016-05-19 21:42:19 +0000

    Changes to condense script; Lower user input

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
 import subprocess
-gunicorn_file = '''import subprocess
-rawinput = raw_input('What port would you like Gunicorn to run on? Default port: 8000')
+rawinput = raw_input('What port would you like Gunicorn to run on? Default port: 8000 \n')
 if(rawinput==''):
-    portToUse = 8000
+    portToUse = str(8000)
 else:
-    portToUse = rawinput
-subprocess.call('gunicorn --bind 0.0.0.0:'+portToUse+' -w 1 "CTFd:create_app()"', shell=True)
-'''
-def gunicornInstall():
-    #subprocess.call(["sudo", "pip", "install", "gunicorn"])
-    print gunicorn_file
-
-gunicornInstall()
+    portToUse = str(rawinput)
+interface = subprocess.check_output('netstat -i | grep BMRU', shell=True).split()[0]
+detectedIP = subprocess.check_output("ifconfig "+interface+" | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'", shell=True)
+print 'Alright! Now, in your web browser, connect to http://'+detectedIP.rstrip()+':'+portToUse
+print ''
+print ''
+subprocess.call('sudo gunicorn --bind 0.0.0.0:'+portToUse+' -w 1 "CTFd:create_app()"', shell=True)
